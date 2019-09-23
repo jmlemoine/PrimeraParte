ac456b09ca5fcc51a5cd723a8a2c07620e9f0122
jmlemoine <jeanmelvinlp27@gmail.com>
2019-09-23 18:52:17 -0400

    Vista del Log In y del Sign Up

---
+++ app/src/main/java/com/example/practica1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.practica1;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void SignUp(View view){
+        Intent signup = new Intent(this, activity_signup.class);
+        startActivity(signup);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/practica1/activity_signup.java
@@ -0,0 +1,19 @@
+package com.example.practica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+
+
+}
