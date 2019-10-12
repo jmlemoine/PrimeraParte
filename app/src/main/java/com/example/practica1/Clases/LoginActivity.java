34ea3847ef25151df3ca80591447c46846b2e662
jmlemoine <jeanmelvinlp27@gmail.com>
2019-10-12 16:49:37 -0400

    Quitando el nombre Inicio en el navbar en el Inicio

--- app/src/main/java/com/example/practica1/Clases/LoginActivity.java
+++ app/src/main/java/com/example/practica1/Clases/LoginActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.practica1.Clases.Inicio.InicioActivityStart;
+import com.example.practica1.Clases.LoginActivity;
 import com.example.practica1.Conexion.ConexionSQLiteHelper;
 import com.example.practica1.Entidades.DBHelper;
 import com.example.practica1.Entidades.Usuario;
@@ -25,10 +26,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Cursor fila;
     conUsuario con;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         usuario = (EditText)findViewById(R.id.txtUsuario);
         clave = (EditText)findViewById(R.id.txtClave);
 
@@ -45,28 +49,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    public void ingresar(View v){
-        DBHelper admin=new DBHelper(this,"BDbdECommercer",null,1);
-        SQLiteDatabase db=admin.getWritableDatabase();
-        String user=usuario.getText().toString();
-        String pass=clave.getText().toString();
-        fila=db.rawQuery("select usuario,clave from Usuario where usuario='"+user+"' and clave='"+pass+"'",null);
-        while(fila.moveToFirst()==true){
-            String usua=fila.getString(0);
-            String contra=fila.getString(1);
-            if (user.equals(usua)&&pass.equals(contra)){
-                Intent ven=new Intent(this,InicioActivity.class);
-                startActivity(ven);
-                usuario.setText("");
-                clave.setText("");
 
-            }
-            else {
-                Toast.makeText(getApplicationContext(), "Usuario incorrecto", Toast.LENGTH_LONG).show();
-
-            }
-        }
-    }
 
     public void onClick(View v){
         switch (v.getId()){
